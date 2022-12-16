cc0d6c595998f284e5c8a9db4eaddae649e97380
dmaldonf <dianacarolina8m@gmail.com>
2022-12-16 16:18:05 -0500

    reestructurar

--- src/main/java/com/nttdata/bootcamp/util/Constant.java
+++ src/main/java/com/nttdata/bootcamp/util/Constant.java
@@ -7,7 +7,7 @@ public class Constant {
     public static final Double COMISSION = 8.5;
     public static final Long COUNT_TRANSACTION = 2l;
 
-    public static final String TYPE_ACCOUNT = "ACTIVE";
+    public static final String TYPE_ACCOUNT = "PASSIVE";
     public static final String STATUS_ACTIVE="ACTIVE";
     public static final String STATUS_INACTIVE="INACTIVE";
 
